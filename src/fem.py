62aa542441bc21a6d1616edc85723d7b6fae2bb0
Jared <jaredthomas68@gmail.com>
2017-10-09 11:09:54 -0600

    implemented error check and exact solution for each forcing function for coding assignment 1

--- src/fem.py
+++ src/fem.py
@@ -90,31 +90,108 @@ def solve_for_displacements(d, Nell, he, g=0):
     x1 = 0.0
 
     u[0] = (1.-x1)*d[0]
-
+    print x1
     for e in np.arange(1, Nell):
-        x1 += he[e]
-        u[e] = u[e-1] + (1.-x1)*d[e]
-        # u[e] = (1.-x1)*d[e]
 
-    u[-1] = u[-2] + g
-    # u[-1] = g
+        x1 += he[e]
+        print x1
+        # u[e] = u[e-1] + (1.-x1)*d[e]
+        # u[e] = (1.-x1)*d[e]
+        u[e] = d[e]
+
+    # u[-1] = u[-2] + g
+    u[-1] = g
 
     return u
 
-def plot_displaccements(u, he, Nell, q=1):
+def get_node_locations_x(Nell, he):
 
-    x_el = np.zeros(Nell+1)
-    x_ex = np.linspace(0, 1., 100)
+    x_el = np.zeros(Nell + 1)
 
     for e in np.arange(1, Nell):
         x_el[e] = x_el[e-1] + he[e-1]
     x_el[Nell] = x_el[Nell-1] + he[Nell-1]
 
-    u_ex = q*(1.-x_ex**3)/6.
+    return x_el
+
+def plot_displaccements(u, x, he, Nell, q=1, ffunc=1):
+
+
+    x_ex = np.linspace(0, 1., 100)
+
+    x_el = get_node_locations_x(Nell, he)
+
+    u_ex = get_u_of_x_exact(x_ex, q, ffunc)
+
+    u_a = get_u_of_x_approx(x, u, he)
 
     plt.plot(x_ex, u_ex)
-    plt.plot(x_el, u, 's')
+    plt.plot(x_el, u, '-s')
+    plt.plot(x, u_a, 'o', markerfacecolor='none')
+
+    plt.xlabel('X Position')
+    plt.ylabel("Displacement")
     plt.show()
+    return
+
+def get_u_of_x_approx(Xp, u, he):
+    Nell = he.size
+    Xn = np.zeros(Nell+1)
+    for e in np.arange(1, Nell+1):
+        Xn[e] = Xn[e-1] + he[e-1]
+
+    u_x = np.zeros_like(Xp)
+    for x, i in zip(Xp, np.arange(0,Xp.size)):
+        for e in np.arange(1, Nell+1):
+            if x < Xn[e]:
+                u_x[i] = ((u[e] - u[e-1])/(Xn[e] - Xn[e-1]))*(x-Xn[e-1]) + u[e-1]
+                break
+
+    return u_x
+
+def get_u_of_x_exact(x, q, ffunc=1):
+
+    u_ex = 0
+
+    if ffunc == 0:
+        u_ex = q*(1-x**2)/2.
+    elif ffunc == 1:
+        u_ex = q*(1.-x**3)/6.
+    elif ffunc == 2:
+        u_ex = q * (1. - x ** 4) / 12.
+
+    return u_ex
+
+def quadrature(Xe, he, ue, ffunc):
+
+    xi = np.array([-np.sqrt(3./5.), 0., np.sqrt(3./5.)])
+    w = np.array([5./9., 8./9., 5./9.])
+
+    error_squared = 0.0
+    for el in np.arange(0, Nell):
+        x = x_of_xi(xi, Xe, he, el)
+
+        dxdxi = he[el]/2
+
+        ux = get_u_of_x_exact(x, q=1, ffunc=ffunc)
+        ua = get_u_of_x_approx(x, ue, he)
+
+        error_squared += np.sum(((ux-ua)**2)*dxdxi*w)
+
+    error = np.sqrt(error_squared)
+
+    return error
+
+def x_of_xi(xi, Xe, he, el):
+
+    x = (he[el]*xi+Xe[el]+Xe[el+1])/2.
+
+    return x
+
+def plot_error():
+
+    
+
     return
 
 if __name__ == "__main__":
@@ -122,6 +199,11 @@ if __name__ == "__main__":
     # input variables
     Nell = 4
     he = np.ones(Nell)/Nell
+    x = np.linspace(0, 1, 4*Nell+1)
+    ffunc = 1
+    Xe = get_node_locations_x(Nell, he)
+    # error
+    # er_4_el = 0.0051
 
     tic = time.time()
     K = define_stiffness_matrix(Nell, he)
@@ -130,7 +212,7 @@ if __name__ == "__main__":
     print "Time to define stiffness matrix: %.3f (s)" % (toc-tic)
 
     tic = time.time()
-    F = define_forcing_vector(Nell, he, ffunc=1)
+    F = define_forcing_vector(Nell, he, ffunc=ffunc)
     toc = time.time()
     print F
     print np.array([1/96., 1./16., 1/8., 3./16.])
@@ -152,7 +234,9 @@ if __name__ == "__main__":
 
     print "Finished"
 
-    plot_displaccements(u, he, Nell)
+    error = quadrature(Xe, he, u, ffunc)
+    print error
+    plot_displaccements(u, x, he, Nell, ffunc=ffunc)
 
 
     # pre compute
